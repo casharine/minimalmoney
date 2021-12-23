f533a1370aa07592afc52b8caedb1c7a07d55a30
casharine <casharine@gmail.com>
2021-12-23 11:37:20 +0900

    変更：seederデータ追加

--- database/seeders/TransactionsTableSeeder.php
+++ database/seeders/TransactionsTableSeeder.php
@@ -19,10 +19,106 @@ class TransactionsTableSeeder extends Seeder
         DB::table('transactions')->insert([
             'book_id' => 1,
             'editor_id' => 1,
+            'transaction_item_id' => 1,
+            'price' => 1000,
+            'date' => 20211223,
+            'note' => '',
+        ]);
+        DB::table('transactions')->insert([
+            'book_id' => 1,
+            'editor_id' => 1,
+            'transaction_item_id' => 2,
+            'price' => 2000,
+            'date' => 20211223,
+            'note' => '',
+        ]);
+        DB::table('transactions')->insert([
+            'book_id' => 1,
+            'editor_id' => 1,
+            'transaction_item_id' => 3,
+            'price' => 3000,
+            'date' => 20211223,
+            'note' => '',
+        ]);
+        DB::table('transactions')->insert([
+            'book_id' => 1,
+            'editor_id' => 1,
+            'transaction_item_id' => 4,
+            'price' => 4000,
+            'date' => 20211223,
+            'note' => '',
+        ]);
+        DB::table('transactions')->insert([
+            'book_id' => 1,
+            'editor_id' => 1,
             'transaction_item_id' => 5,
-            'price' => 100,
-            'date' => 20211116,
-            'note' => '水',
+            'price' => 5000,
+            'date' => 20211223,
+            'note' => '',
+        ]);
+        DB::table('transactions')->insert([
+            'book_id' => 1,
+            'editor_id' => 1,
+            'transaction_item_id' => 6,
+            'price' => 6000,
+            'date' => 20211223,
+            'note' => '',
+        ]);
+        DB::table('transactions')->insert([
+            'book_id' => 1,
+            'editor_id' => 1,
+            'transaction_item_id' => 7,
+            'price' => 7000,
+            'date' => 20211223,
+            'note' => '',
+        ]);
+        DB::table('transactions')->insert([
+            'book_id' => 1,
+            'editor_id' => 1,
+            'transaction_item_id' => 8,
+            'price' => 8000,
+            'date' => 20211223,
+            'note' => '',
+        ]);
+        DB::table('transactions')->insert([
+            'book_id' => 1,
+            'editor_id' => 1,
+            'transaction_item_id' => 9,
+            'price' => 9000,
+            'date' => 20211223,
+            'note' => '',
+        ]);
+        DB::table('transactions')->insert([
+            'book_id' => 1,
+            'editor_id' => 1,
+            'transaction_item_id' => 10,
+            'price' => 10000,
+            'date' => 20211223,
+            'note' => '',
+        ]);
+        DB::table('transactions')->insert([
+            'book_id' => 1,
+            'editor_id' => 1,
+            'transaction_item_id' => 11,
+            'price' => 11000,
+            'date' => 20211223,
+            'note' => '',
+        ]);
+        DB::table('transactions')->insert([
+            'book_id' => 1,
+            'editor_id' => 1,
+            'transaction_item_id' => 12,
+            'price' => 12000,
+            'date' => 20211223,
+            'note' => '',
+        ]);
+        DB::table('transactions')->insert([
+            'book_id' => 1,
+            'editor_id' => 1,
+            'transaction_item_id' => 13,
+            'price' => 13000,
+            'date' => 20211223,
+            'note' => '',
         ]);
     }
 }
